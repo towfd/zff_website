be2580274e669fff7298439b354b2aa2da8d8266
towfd <karta54637666@gmail.com>
2026-04-29 23:07:08 +0800

    open website

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/zff_website/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
